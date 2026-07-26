0a79e0fa76638fcf07ffd03dbc386811fbb196cb
TungDuong-Dev <devopptungduong@gmail.com>
2026-07-26 21:27:16 +0700

    Fix: RegEx Password,CCCD

--- Java/src/utils/constant/RegexConstant.java
+++ Java/src/utils/constant/RegexConstant.java
@@ -2,8 +2,8 @@ package utils.constant;
 
 public final class RegexConstant {
     public static final String EMAIL = "^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})$";
-    public static final String CCCD = "^(00[1-9]|[1-8][0-9]|9[0-6])\\d{9}$";
-    public static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{6,}$";
+    public static final String CCCD = "^(00[1-9]|0[1-8][0-9]|09[0-6])\\d{9}$";
+    public static final String PASSWORD ="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&!_])[A-Za-z\\d@#$%^&!_]{6,20}$";
     public static final String PHONE = "^(03|05|07|09)\\d{8}$";
 
     private RegexConstant() {}
